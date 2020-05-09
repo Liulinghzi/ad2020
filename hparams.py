d133ca37902d48d31e7904ceff64745663216ee3
liulingzhi <136617371@qq.com>
2020-05-09 17:53:43 +0800

    add params

--- hparams.py
+++ hparams.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2019-09-23 18:54:24
-@LastEditTime: 2020-05-09 16:17:49
+@LastEditTime: 2020-05-09 17:53:27
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /transformer-master/hparams.py
@@ -18,7 +18,9 @@ class Hparams:
 
     # train
     ## files
-    parser.add_argument('--train', default='iwslt2016/segmented/train.de.bpe',
+    parser.add_argument('----train_dense_path:', default='iwslt2016/segmented/train.de.bpe',
+                             help="german training segmented data")
+    parser.add_argument('----train_sparse_path:', default='iwslt2016/segmented/train.de.bpe',
                              help="german training segmented data")
     parser.add_argument('--train_age_gender', default='iwslt2016/segmented/train.de.bpe',
                              help="german training segmented data")
