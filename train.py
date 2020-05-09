ce9be2eb4555bf7fedff6841f793c19ac34513f9
liulingzhi <136617371@qq.com>
2020-05-09 18:30:17 +0800

    d

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2019-09-23 18:54:24
-@LastEditTime: 2020-05-09 18:29:44
+@LastEditTime: 2020-05-09 18:30:15
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /transformer-master/train.py
@@ -36,8 +36,6 @@ save_hparams(hp, hp.logdir)
 
 
 logging.info("# Prepare train/eval batches")
-print('param')
-exit()
 train_batches, num_train_batches, num_train_samples = get_batch(hp.train_dense_path, hp.train_sparse_path, hp.train_age_gender_path, hp.maxlen, hp.batch_size, shuffle=True)
 # eval_batches, num_eval_batches, num_eval_samples = get_batch(hp.eval_dense_path, hp.eval_sparse_path, hp.eval_age_gender_path, 100000 , hp.batch_size, shuffle=False)
 print('get batch')
