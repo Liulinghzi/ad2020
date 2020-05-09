7b2bc547be780382c73d95b3dce77dadaf06def1
liulingzhi <136617371@qq.com>
2020-05-09 18:22:24 +0800

    demo.sh

--- demo.sh
+++ demo.sh
@@ -1,38 +1,39 @@
 ###
  # @Author: your name
  # @Date: 2020-05-09 15:28:53
- # @LastEditTime: 2020-05-09 18:18:06
+ # @LastEditTime: 2020-05-09 18:22:14
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /ad2020/run.sh
  ###
 
 
-python3 train.py \
+python3 train.py  --train_dense_path ../5-8/sample_train_dense_seqs.pkl  --train_sparse_path ../5-8/sample_train_sparse_seqs.pkl --train_age_gender_path ../5-8/sample_train_age_gender.pkl
+
 
 # train
 ## files
-    --train_dense_path ../5-8/sample_train_dense_seqs.pkl\
-    --train_sparse_path ../5-8/sample_train_sparse_seqs.pkl\
-    --train_age_gender_path ../5-8/sample_train_age_gender.pkl\
-# # test
-# --test\
-# --ckpt\
-# --testdir\
-# training scheme
-    --batch_size 128\
-# --lr\
-# --warmup_steps\
-# --logdir\
-    --num_epochs 1\
+    # --train_dense_path ../5-8/sample_train_dense_seqs.pkl\
+    # --train_sparse_path ../5-8/sample_train_sparse_seqs.pkl\
+    # --train_age_gender_path ../5-8/sample_train_age_gender.pkl\
+# # # test
+# # --test\
+# # --ckpt\
+# # --testdir\
+# # training scheme
+#     --batch_size 128\
+# # --lr\
+# # --warmup_steps\
+# # --logdir\
+#     --num_epochs 1\
 
-# model
-    --d_model 32\
-    --d_ff 32\
-    --num_blocks 1\
-    --num_heads 1\
-    --maxlen 50\
-    --dropout_rate\
-    # --smoothing\
-    --age_classes 2\
-    --gender_classes 10
+# # model
+#     --d_model 32\
+#     --d_ff 32\
+#     --num_blocks 1\
+#     --num_heads 1\
+#     --maxlen 50\
+#     --dropout_rate\
+#     # --smoothing\
+#     --age_classes 2\
+#     --gender_classes 10
