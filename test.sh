a5cc15dd38d8c222c64b152c0487db3a335a455c
liulingzhi <136617371@qq.com>
2020-05-12 20:24:01 +0800

    test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+###
+ # @Author: your name
+ # @Date: 2020-05-12 20:22:55
+ # @LastEditTime: 2020-05-12 20:23:53
+ # @LastEditors: your name
+ # @Description: In User Settings Edit
+ # @FilePath: /ad2020/test.sh
+ ###
+python3 test.py  --test_features_path ../5-8/test_features.pkl  --test_labels_path ../5-8/test_labels.pkl --vocab_list ../5-8/vocab_size_list.pkl --num_heads 2 --d_model 4  --ckpt log/1
