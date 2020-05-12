6221633e90d00a661bea4324edee930080aa1aff
liulingzhi <136617371@qq.com>
2020-05-12 15:23:06 +0800

    bug

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2019-09-23 18:54:24
-@LastEditTime: 2020-05-12 15:22:04
+@LastEditTime: 2020-05-12 15:23:02
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /transformer-master/train.py
@@ -81,7 +81,7 @@ with tf.Session() as sess:
             _loss = sess.run(loss) # train loss
             # cpred_age, cpred_gender = sess.run([pred_age, pred_gender])
             # clables = sess.run([labels])
-            print(sess.run(age_gender_logits,age_gender_))
+            print(sess.run([age_gender_logits,age_gender_]))
 
             # print(cpred_age[:5])
             # print(cpred_gender[:5])
