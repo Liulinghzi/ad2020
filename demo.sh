94fb3b556c9cda8ced75419bcc2018894776f3f4
liulingzhi <136617371@qq.com>
2020-05-12 14:58:20 +0800

    demo指定GPU

--- demo.sh
+++ demo.sh
@@ -1,14 +1,14 @@
 ###
  # @Author: your name
  # @Date: 2020-05-09 15:28:53
- # @LastEditTime: 2020-05-12 14:57:31
+ # @LastEditTime: 2020-05-12 14:58:06
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /ad2020/run.sh
  ###
 
 
-python3 train.py  --train_features_path ../../5-8/train_features.pkl  --train_labels_path ../../5-8/train_labels.pkl --vocab_list ../../5-8/vocab_size_list.pkl --num_heads 2 --d_model 4\
+CUDA_VISIBLE_DEVICES=1 python3 train.py  --train_features_path ../../5-8/train_features.pkl  --train_labels_path ../../5-8/train_labels.pkl --vocab_list ../../5-8/vocab_size_list.pkl --num_heads 2 --d_model 4\
 
 
 
