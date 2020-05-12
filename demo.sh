f1fa337ae6de2db886690cd4a525aeb6ecfcca86
liulingzhi <136617371@qq.com>
2020-05-12 12:17:07 +0800

    demo.sh

--- demo.sh
+++ demo.sh
@@ -1,14 +1,14 @@
 ###
  # @Author: your name
  # @Date: 2020-05-09 15:28:53
- # @LastEditTime: 2020-05-12 10:04:41
+ # @LastEditTime: 2020-05-12 12:16:01
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /ad2020/run.sh
  ###
 
 
-python3 train.py  --train_features_path ../5-8/sample_train_features.pkl  --train_labels_path ../5-8/sample_train_labels.pkl
+python3 train.py  --train_features_path ../5-8/sample_train_features.pkl  --train_labels_path ../5-8/sample_train_labels.pkl --vocab_list ../5-8/vocab_size_list.pkl
 
 
 # train
