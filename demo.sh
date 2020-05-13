4f55c39c2c47f11280d41b3ea3c65d953d57e5c1
liulingzhi <136617371@qq.com>
2020-05-13 10:37:58 +0800

    调参

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: your name
  # @Date: 2020-05-09 15:28:53
- # @LastEditTime: 2020-05-13 10:33:35
+ # @LastEditTime: 2020-05-13 10:35:40
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /ad2020/run.sh
@@ -9,7 +9,7 @@
 
 
 # CUDA_VISIBLE_DEVICES=1 python3 train.py  --train_features_path ../../5-8/sample_train_features.pkl  --train_labels_path ../../5-8/sample_train_labels.pkl --vocab_list ../../5-8/sample_vocab_size_list.pkl --num_heads 2 --d_model 64 --num_blocks 3 --d_ff 512
-CUDA_VISIBLE_DEVICES=1 python3 train.py  --train_features_path ../../5-8/train_features.pkl  --train_labels_path ../../5-8/train_labels.pkl --vocab_list ../../5-8/vocab_size_list.pkl --num_heads 2 --d_model 4\
+CUDA_VISIBLE_DEVICES=1 python3 train.py  --train_features_path ../../5-8/train_features.pkl  --train_labels_path ../../5-8/train_labels.pkl --vocab_list ../../5-8/vocab_size_list.pkl --num_heads 2 --d_model 64 --num_blocks 3 --d_ff 512\
 
 
 
